95bbfafed3d831d4362ce40c34639fd92f1d90d5
Vijay Kumar <wijay.outlook.in>
2023-02-16 18:36:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("script is working from a different file");
+// var username = prompt("Give me username:")
+// alert("This script works: " + username);
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!");
+});
