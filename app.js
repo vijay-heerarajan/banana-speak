3a21421539cdf7b3be849c6cda99974cfea21cf6
Vijay Kumar <wijay.outlook.in>
2023-02-18 21:55:37 +0530

    basic translation app done

--- app.js
+++ app.js
@@ -6,15 +6,37 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
 // outputDiv.innerText = "vija";
 
 console.log(txtInput);
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server: try again after some time");
+}
 
 function clickHandler() {
         // console.log("clicked!");
         // console.log("click", txtInput.value);
 
-        outputDiv.innerText = "abcefflljalfa" + txtInput.value;
+        // outputDiv.innerText = "abcefflljalfa" + txtInput.value;
+
+        var inputText = txtInput.value;
+
+        fetch(getTranslationURL(inputText))
+          .then(response => response.json())
+          .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+          .catch(errorHandler);
       }
 
 btnTranslate.addEventListener("click", clickHandler);
